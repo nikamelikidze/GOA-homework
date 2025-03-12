68be4dc43adfc21355aa2b53a0c0d5b52574ce5e
Nikamelikidze <nikamelikidze2022@gmail.com>
2025-03-12 16:04:01 +0400

    commit

---
+++ level8/homework/main.py
@@ -0,0 +1,29 @@
+Integer (მთელი რიცხვი)
+age = 25
+
+Float (ათწილადი რიცხვი)
+height = 1.75
+
+String (სტრიქონი, ტექსტი)
+name = "მარიამი"
+
+Boolean (ბულეანი, True ან False მნიშვნელობა)
+is_student = True
+
+List (სია)
+numbers = [1, 2, 3, 4, 5]
+
+Tuple (კორტეჟი, უცვლელი სია)
+coordinates = (41.69, 44.80)
+
+Dictionary (ლექსიკონი, key-value წყვილები)
+person = {"name": "ლაშა", "age": 30}
+
+Set (მრავლობა, უნიკალური ელემენტების კრებული)
+unique_numbers ={1, 2, 3, 4}
+
+
+
+print(word1 + " " + word2 + " " + word3 + ".")  # "მე მიყვარს პროგრამირება."
+print(word4 + " ძალიან საინტერესოა!")  # "Python ძალიან საინტერესოა!"
+print(word1 + " ვსწავლობ " + word4 + "-ს.")  # "მე ვსწავლობ Python-ს."
---
+++ level9/homework/main.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ka">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>გოა - საოცარი დასვენების ადგილი</title>
+</head>
+<body>
+    <h1>გოა</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/d/d2/Palolem_Beach_Goa.jpg" alt="გოას სანაპირო" width="400">
+    <p>გოა არის ინდოეთის ერთ-ერთი ყველაზე პოპულარული ტურისტული მიმართულება, რომელიც ცნობილია თავისი ულამაზესი პლაჟებით და ღამის ცხოვრებით.</p>
+</body>
+</html>
