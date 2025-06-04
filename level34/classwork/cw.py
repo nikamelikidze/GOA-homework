9cb58ced2405478dcfdc83d2800045e2d6931b57
Nikamelikidze <nikamelikidze2022@gmail.com>
2025-06-04 20:26:10 +0400

    commit

---
+++ level34/classwork/cw.py
@@ -0,0 +1,14 @@
+# 1) სიის შექმნა 5 ელემენტით,
+my_list = ["apple", "banana", "cherry", "date", "fig"]
+
+# 2) მესამე ელემენტის ამოგდება (index 2 — "cherry"),
+my_list.pop(2)
+
+# 3) მეორე ინდექსზე (index 2) ჩასმა ელემენტის — 5,
+my_list.insert(2, 5)
+
+# 4) სიის ბოლოში "Gurami"-ს დამატება,
+my_list.append("Gurami")
+
+# 5) სიის სიგრძის დაბეჭდვა,
+print(len(my_list))
